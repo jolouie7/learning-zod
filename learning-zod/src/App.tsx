d0fb630e0802c2b0eab998b5b8f227bebd69b571
Joseph <joseph161216@gmail.com>
2023-01-18 20:16:43 -0800

    Add basic primatives

--- learning-zod/src/App.tsx
+++ learning-zod/src/App.tsx
@@ -2,11 +2,16 @@ import { useState } from "react";
 import { z } from "zod";
 
 function App() {
-  const UserSchema = z.object({ username: z.string() });
+  const UserSchema = z.object({
+    username: z.string(), //string
+    age: z.number().optional(), //number
+    birthday: z.date().optional(), //date
+    isProgrammer: z.boolean().optional(), //boolean
+  });
 
   const user = { username: "Alex" };
 
-  console.log(UserSchema.safeParse(user));
+  console.log(UserSchema.safeParse(user)); //sageParse return if successful and the data
 
   return <div>Hello world</div>;
 }
